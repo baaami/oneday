7d69c313a6845101e69d8812b464d90f99c4c7f8
baaami <lgh121546@naver.com>
2022-10-09 23:46:37 +0900

    CORS ERROR 해결

--- main.go
+++ main.go
@@ -11,9 +11,25 @@ const port string = ":4000"
 
 func main() {
 	router := gin.Default()
-
+	router.Use(CORSMiddleware())
 	api.SetRouter(router)
 	router.Run(port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080"
 
 	db.CloseDB()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Methods", "GET, DELETE, POST, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
